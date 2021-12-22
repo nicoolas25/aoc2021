3c746f727db613a2f96c41262a52dc24bb51637a
Nicolas Zermati <nicoolas25@gmail.com>
2021-12-22 21:20:32 +0100

    day_10.rb Use classes in Ruby

--- day10/part2.rb
+++ day10/part2.rb
@@ -1,19 +1,19 @@
 require "./part1"
 
-SCORES = { "(" => 1, "[" => 2, "{" => 3, "<" => 4 }
-
-def score(opening_chars_to_close)
-  opening_chars_to_close.reduce(0) do |total, char|
-    total * 5 + SCORES.fetch(char)
-  end
+# Reopen the class to add an accessor for remaining closing_chars
+class Line
+  attr_reader :closing_chars
 end
 
 if __FILE__ == $0
-  lines = ARGF.each_line.map { |line| line.chomp.each_char.to_a }
-  incomplete_scores = lines.map.with_index do |line, index|
-    score(read_line(line, index + 1).reverse)
-  rescue CorruptedLineError
-    nil
-  end.compact
+  scores = { ")" => 1, "]" => 2, "}" => 3, ">" => 4 }
+  incomplete_scores = ARGF.each_line
+    .map { |line| Line.new(line.chomp) }
+    .reject { |line| line.corrupted? || line.closing_chars.empty? }
+    .map do |line|
+      line.closing_chars.reduce(0) do |total, char|
+        total * 5 + scores.fetch(char)
+      end
+    end
   puts(incomplete_scores.sort[incomplete_scores.size / 2])
 end
